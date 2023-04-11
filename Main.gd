5625886c64cded60e9c93fcdac02ffd52b25cd72
Marcelo Cohen <marcelo.cohen@pucrs.br>
2023-04-11 09:27:11 -0300

    Projeto inicial com movimento p/ direita

---
+++ Main.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+const SPEED : = 300
+var total : float = 0
+
+func _ready() -> void:
+	update_score(total)
+
+func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("right"):
+	#	position.x += 5
+	print("Right!")
+		
+func _physics_process(delta: float) -> void:
+	total += delta
+	#print(delta)
+	update_score(total)
+	if Input.is_action_pressed("right"):
+		position.x += SPEED * delta
+	
+func update_score(current_score: float) -> void:
+	$Score.text = str(total)
+			
